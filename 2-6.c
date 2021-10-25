f8ad6c37e52237df3969bbe205adb532a0e45632
hojungkwak <92517416+hojungkwak@users.noreply.github.com>
2021-10-25 17:10:39 +0900

    Add files via upload

---
+++ 2-6.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+void foun(void);
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a += 3)
+	{
+		foun();
+	} return 0;
+}
+void foun(void)
+{
+	int n;
+	for (n = 0; n < 20; n += 3)
+		printf("!");
+	printf("\n");
+}
